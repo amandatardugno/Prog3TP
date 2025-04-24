21f120936921d3fd6209069f22568018936694a0
Amanda <amandatardugno@gmail.com>
2025-04-24 19:48:45 -0300

    UCS Implementation

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -1,5 +1,5 @@
 from ..models.grid import Grid
-from ..models.frontier import StackFrontier
+from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -15,13 +15,34 @@ class UniformCostSearch:
         Returns:
             Solution: Solution found
         """
-        # Initialize a node with the initial position
         node = Node("", grid.start, 0)
 
-        # Initialize the explored dictionary to be empty
-        explored = {} 
-        
-        # Add the node to the explored dictionary
-        explored[node.state] = True
-        
+        if node.state == grid.end:
+            return Solution(node, reached={node.state: 0})
+
+        frontier = PriorityQueueFrontier()
+        frontier.add(node, priority=0)
+
+        explored = {node.state: 0}
+
+        while frontier.frontier:
+            node = frontier.pop()
+
+            if node.state == grid.end:
+                return Solution(node, explored)
+
+            for action, new_state in grid.get_neighbours(node.state).items():
+                new_cost = node.cost + grid.get_cost(new_state)
+
+                if new_state not in explored or new_cost < explored[new_state]:
+                    explored[new_state] = new_cost
+                    new_node = Node(
+                        "",
+                        state=new_state,
+                        cost=new_cost,
+                        parent=node,
+                        action=action
+                    )
+                    frontier.add(new_node, priority=new_cost)
+
         return NoSolution(explored)
