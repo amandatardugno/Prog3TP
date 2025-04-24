d1b898edf7a4766970d745ef0cdd3412dd534241
Amanda <amandatardugno@gmail.com>
2025-04-24 19:48:16 -0300

    DFS Implementation

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -15,13 +15,36 @@ class DepthFirstSearch:
         Returns:
             Solution: Solution found
         """
-        # Initialize a node with the initial position
         node = Node("", grid.start, 0)
 
-        # Initialize the explored dictionary to be empty
+        if node.state == grid.end:
+            return Solution(node, reached={node.state: True})
+
         explored = {} 
         
-        # Add the node to the explored dictionary
         explored[node.state] = True
-        
+
+        frontier = StackFrontier()
+        frontier.add(node)
+
+        while not frontier.is_empty():
+            node = frontier.remove()
+
+            for action, new_state in grid.get_neighbours(node.state).items():
+                if new_state not in explored:
+                    new_node = Node(
+                        "", 
+                        state=new_state,
+                        cost=node.cost + grid.get_cost(new_state),
+                        parent=node,
+                        action=action
+                    )
+
+                    if new_state == grid.end:
+                        return Solution(new_node, explored)
+
+                    explored[new_state] = True
+                    frontier.add(new_node)
+
         return NoSolution(explored)
+
